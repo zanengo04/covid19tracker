b4ae7b34071ef302854e00338425505318e50b34
Quoc Ngo <zanengo04@gmail.com>
2020-12-30 15:41:48 -0500

    Updates

---
+++ precache-manifest.f285181edd5eecacb50f344e32de53a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b40f701a28011a510ac001f0615dedcb",
+    "url": "/covid19tracker/index.html"
+  },
+  {
+    "revision": "67af91ab9630585de786",
+    "url": "/covid19tracker/static/css/main.273d087a.chunk.css"
+  },
+  {
+    "revision": "ee0842d3a4356a3245ae",
+    "url": "/covid19tracker/static/js/2.5ee75e06.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/covid19tracker/static/js/2.5ee75e06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67af91ab9630585de786",
+    "url": "/covid19tracker/static/js/main.64da3593.chunk.js"
+  },
+  {
+    "revision": "037a348b64b8cf25591f",
+    "url": "/covid19tracker/static/js/runtime-main.1a2095c6.js"
+  }
+]);
